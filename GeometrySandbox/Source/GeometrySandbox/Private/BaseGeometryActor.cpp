b6c980970c71a21b447e745bde55188b56b8fbbf
Oleksandr <brovarsv@gmail.com>
2021-02-17 23:21:10 +0200

    Lesson 9. Макрос UPROPERTY

--- GeometrySandbox/Source/GeometrySandbox/Private/BaseGeometryActor.cpp
+++ GeometrySandbox/Source/GeometrySandbox/Private/BaseGeometryActor.cpp
@@ -7,50 +7,38 @@
 DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
 
 // Sets default values
-ABaseGeometryActor::ABaseGeometryActor()
-{
+ABaseGeometryActor::ABaseGeometryActor() {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
 // Called when the game starts or when spawned
-void ABaseGeometryActor::BeginPlay()
-{
+void ABaseGeometryActor::BeginPlay() {
 	Super::BeginPlay();
 
-	//PrintTypec();
+	 //PrintStringTypes();
+	 PrintTypes();
 
 }
 
 // Called every frame
-void ABaseGeometryActor::Tick(float DeltaTime)
-{
+void ABaseGeometryActor::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
 }
 
 void ABaseGeometryActor::PrintTypes() {
-	int WeaponsNum = 4;
-	int KillsNum = 7;
-	float Health = 34.435235f;
-	bool IsDead = false;
-	bool HasWeapon = true;
-
-	UE_LOG(LogTemp, Display, TEXT("Weapons num: %d, kills num: %i"), WeaponsNum, KillsNum);
-	UE_LOG(LogTemp, Display, TEXT("Heals: %f"), Health);
-	UE_LOG(LogTemp, Display, TEXT("Heals: %.2f"), Health);
-	UE_LOG(LogTemp, Display, TEXT("IsDead: %d"), IsDead);
-	UE_LOG(LogTemp, Display, TEXT("HasWeapon: %d"), static_cast<int>(HasWeapon));
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Actor name %s"), *GetName());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Weapons num: %d, kills num: %i"), WeaponsNum, KillsNum);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Heals: %f"), Health);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("IsDead: %d"), IsDead);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("HasWeapon: %d"), static_cast<int>(HasWeapon));
 }
 
-void ABaseGeometryActor::PrintStringType() {
+void ABaseGeometryActor::PrintStringTypes() {
 	FString Name = "John Connor";
 	UE_LOG(LogBaseGeometry, Display, TEXT("Name: %s"), *Name);
-
-	int WeaponsNum = 4;
-	float Health = 34.435235f;
-	bool IsDead = false;
 
 	FString WeaponsNumStr = "Weapons num = " + FString::FromInt(WeaponsNum);
 	FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
